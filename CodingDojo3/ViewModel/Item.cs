51e36f26a4f86af01499acf33dcdcd345521ac22
Nadja Reifschneider <wi15b032@technikum-wien.at>
2016-11-26 16:45:00 +0100

    Daten einbinden

---
+++ CodingDojo3/ViewModel/Item.cs
@@ -0,0 +1,87 @@
+﻿using CodingDojo4DataLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo3.ViewModel
+{
+    public class Item
+    {
+        private StockEntry entry;
+
+        public Item(StockEntry stEntry)
+        {
+            entry = stEntry;
+        }
+
+
+        public string Name
+        {
+            get
+            {
+                return entry.SoftwarePackage.Name;
+            }
+
+            set
+            {
+                entry.SoftwarePackage.Name = value;
+            }
+        }
+
+        public Group Group
+        {
+            get
+            {
+                return entry.SoftwarePackage.Category;
+            }
+
+            set
+            {
+                entry.SoftwarePackage.Category = value;
+            }
+        }
+
+        public double SalesPrice
+        {
+            get
+            {
+                return entry.SoftwarePackage.SalesPrice;
+            }
+
+            set
+            {
+                entry.SoftwarePackage.SalesPrice = value;
+            }
+        }
+
+        public double PurchasePrice
+        {
+            get
+            {
+                return entry.SoftwarePackage.PurchasePrice;
+            }
+
+            set
+            {
+                entry.SoftwarePackage.PurchasePrice = value;
+            }
+        }
+
+        /*public int OnStock
+        {
+            get
+            {
+                return entry.SoftwarePackage;
+            }
+
+            set
+            {
+                onStock = value;
+            }
+        }*/
+
+        
+    }
+}
---
+++ CodingDojo3/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CodingDojo4DataLib;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo3.ViewModel
+{
+    class MainViewModel
+    {
+
+        private List<StockEntry> stocklist;
+
+        private ObservableCollection<Item> itemList = new ObservableCollection<Item>();
+
+        public ObservableCollection<Item> ItemList
+        {
+            get
+            {
+                return itemList;
+            }
+
+            set
+            {
+                itemList = value;
+            }
+        }
+
+        public MainViewModel()
+        {
+            SampleManager manager = new SampleManager();
+            stocklist = manager.CurrentStock.OnStock;
+
+            foreach(var i in manager.CurrentStock.OnStock)
+            {
+                itemList.Add(new Item(i));
+            }
+            
+        }
+
+    }
+}
