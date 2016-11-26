a31328f069fa01da1e10b11c685d6a1070ac43be
Nadja Reifschneider <wi15b032@technikum-wien.at>
2016-11-26 20:03:49 +0100

    Delete Funktion

--- CodingDojo3/ViewModel/MainViewModel.cs
+++ CodingDojo3/ViewModel/MainViewModel.cs
@@ -5,15 +5,23 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace CodingDojo3.ViewModel
 {
-    class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
 
         private List<StockEntry> stocklist;
 
         private ObservableCollection<Item> itemList = new ObservableCollection<Item>();
+        //private ObservableCollection<Item> reducedItemList = new ObservableCollection<Item>();
+
+        private Item selectedItem;
+
+        private RelayCommand deleteBtnClickedCommand;
+       
 
         public ObservableCollection<Item> ItemList
         {
@@ -28,17 +36,66 @@ namespace CodingDojo3.ViewModel
             }
         }
 
+        public Item SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+
+            set
+            {
+                selectedItem = value;
+            }
+        }
+
+        public RelayCommand DeleteBtnClickedCommand
+        {
+            get
+            {
+                return deleteBtnClickedCommand;
+            }
+
+            set
+            {
+                deleteBtnClickedCommand = value;
+            }
+        }
+
+
+        /*
+        public ObservableCollection<Item> ReducedItemList
+        {
+            get
+            {
+                return reducedItemList;
+            }
+
+            set
+            {
+                reducedItemList = value;
+            }
+        }
+        */
+
         public MainViewModel()
         {
             SampleManager manager = new SampleManager();
             stocklist = manager.CurrentStock.OnStock;
 
-            foreach(var i in manager.CurrentStock.OnStock)
+
+            DeleteBtnClickedCommand = new RelayCommand(new Action(DeleteItem));
+
+            foreach (var i in stocklist)
             {
                 itemList.Add(new Item(i));
             }
-            
         }
 
+        private void DeleteItem()
+        {
+            itemList.Remove(selectedItem);
+            selectedItem = null;
+        }
     }
 }
